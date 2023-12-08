012c3bcbc3a5394ffd1ba0e12ad507726a322c04
hiano7972 <hiano7972@gmail.com>
2023-12-08 10:21:54 +0900

    0.2.1 -add bg

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -26,8 +26,14 @@ function MainView() {
     return (
         <>
             <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E]p-10 flex flex-col justify-center items-center">
-                <Image className="w-full h-full absolute" src={""} alt="mainBackGround" />
-                <div className="w-12 h-12 rounded-full fixed bg-red-200 top-4 right-4">btn</div>
+                <Image
+                    className="w-full h-full absolute"
+                    src={"/snow.gif"}
+                    width={100}
+                    height={100}
+                    alt="mainBackGround"
+                />
+                {/* <div className="w-12 h-12 rounded-full fixed bg-red-200 top-4 right-4">btn</div> */}
 
                 <Card className="max-w-4xl mw-auto shadow-xl">
                     <p className="text-7xl font-bold text-center mb-6">Todays Weather</p>
